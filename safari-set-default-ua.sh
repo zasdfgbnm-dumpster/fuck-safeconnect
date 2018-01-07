d8399634116372b3315d12f44a1632f5e86525bf
Xiang Gao <qasdfgtyuiop@gmail.com>
2018-01-07 14:49:23 -0500

    chrome on linux ua

--- safari-set-default-ua.sh
+++ safari-set-default-ua.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-defaults write com.apple.Safari CustomUserAgent "\"$@\""
+defaults write com.apple.Safari CustomUserAgent "\"$(curl https://raw.githubusercontent.com/zasdfgbnm/fuck-safeconnect/master/chrome-on-linux-ua.txt)\""
